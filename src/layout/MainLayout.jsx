53c4b347c370cd8f709995b480fb5e33c7726cc3
TasmiaTory37 <torytasmmiaakter@gmail.com>
2025-05-07 12:12:15 +0600

    Router Setup Complete

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import { Outlet } from 'react-router';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <nav>
+                <Navbar></Navbar>
+                
+            </nav>
+
+            <section>
+               <Outlet></Outlet>
+            </section>
+
+            <footer>
+                <Footer></Footer>
+
+
+            </footer>
+            
+        </div>
+    );
+};
+
+export default MainLayout;
